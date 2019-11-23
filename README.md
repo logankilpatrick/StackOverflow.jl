20a91f3a5e8ac44b0b731e3daf4bd820ba43455d
Logan Kilpatrick <lok026@g.harvard.edu>
2019-11-23 16:55:29 -0500

    Added MWE and readme updates.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Stackoverflow
+
+This package is designed as a wrapped for the [StackOverflow API](https://api.stackexchange.com).
+
+Support from the community would be welcomed in order to expedite coverage of all of the basic abilities.
+
+
+## Usage
+
+```julia
+
+julia> using Stackoverflow
+
+julia> qHolder = getrecentquestionsfortag()
+
+julia> qHolder[1]
+Questions(Any["https://stackoverflow.com/questions/59010720/how-to-make-a-request-to-a-specific-url-in-julia"], Any[15], Any[1574532755], Any[true], Any[Dict{String,Any}("user_type" => "registered","display_name" => "logankilpatrick","reputation" => 2135,"user_id" => 7619808,"link" => "https://stackoverflow.com/users/7619808/logankilpatrick","profile_image" => "https://i.stack.imgur.com/gHvtS.png?s=128&g=1")], Any[1574534113], Any[0], Any[59010920], Any[59010720], Any[Any["julia"]], Any["How to make a request to a specific URL in Julia?"], Any[1])
+
+julia> qHolder[1].link
+1-element Array{Any,1}:
+ "https://stackoverflow.com/questions/59010720/how-to-make-a-request-to-a-specific-url-in-julia"
+
+julia> qHolder[1].view_count
+1-element Array{Any,1}:
+ 15
+
+julia> qHolder[1].owner
+1-element Array{Any,1}:
+ Dict{String,Any}("user_type" => "registered","display_name" => "logankilpatrick","reputation" => 2135,"user_id" => 7619808,"link" => "https://stackoverflow.com/users/7619808/logankilpatrick","profile_image" => "https://i.stack.imgur.com/gHvtS.png?s=128&g=1")
+
+julia> qHolder[1].score
+1-element Array{Any,1}:
+ 0
+
+julia> qHolder[1].title
+1-element Array{Any,1}:
+ "How to make a request to a specific URL in Julia?"
+
+julia> qHolder[1].answer_count
+1-element Array{Any,1}:
+ 1
+
+julia> qHolder[2]
+Questions(Any["https://stackoverflow.com/questions/58956518/how-can-i-view-profiling-information-visually"], Any[145], Any[1574259575], Any[true], Any[Dict{String,Any}("user_type" => "registered","display_name" => "Marouane1994","reputation" => 173,"user_id" => 12078089,"link" => "https://stackoverflow.com/users/12078089/marouane1994","profile_image" => "https://www.gravatar.com/avatar/67241161f774e0fd40923e37c370fb26?s=128&d=identicon&r=PG&f=1")], Any[1574519259], Any[0], Any[], Any[58956518], Any[Any["julia"]], Any["How can I view profiling information visually?"], Any[1])
+```
--- src/requests.jl
+++ src/requests.jl
@@ -1,9 +1,106 @@
 using HTTP
 using Revise
+using CodecZlib
+using JSON
 
-r = HTTP.request("GET", "https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&tagged=Julia&site=stackoverflow"; verbose=3)
-println(typeof(r))
-r = Vector(r)
+mutable struct Questions
+    link::Vector
+    view_count::Vector
+    creation_date::Vector
+    is_answered::Vector
+    owner::Vector
+    last_activity_date::Vector
+    score::Vector
+    accepted_answer_id::Vector
+    question_id::Vector
+    tags::Vector
+    title::Vector
+    answer_count::Vector
+end
 
-#println(r.status)
-#println(String(r.body))
+"""
+    getrecentquestionsfortag(tag::String = "Julia", site::String = "stackoverflow", order::String = "desc",
+                                    sort::String = "activity")
+
+Will return the 30 StackOverflow questions given a certain tag and sort parameter.
+
+Possible order values: "desc" or "asc"
+Possible sort values: "activity", "votes", "creation", "hot", "week", or "month"
+"""
+function getrecentquestionsfortag(;tag::String = "Julia", site::String = "stackoverflow", order::String = "desc",
+                                    sort::String = "activity", todate::String = "", fromdate::String = "")
+
+    questionholder = Vector()
+
+    if fromdate != "" && todate != ""
+        datequery = "fromdate=$(fromdate)&todate=$(todate)&"  #?fromdate=1573776000&todate=1574467200&order
+        r = HTTP.request("GET", "https://api.stackexchange.com/2.2/questions?$(datequery)order=$(order)&sort=$(sort)&tagged=$(tag)&site=$(site)"; verbose=3)
+    else
+        r = HTTP.request("GET", "https://api.stackexchange.com/2.2/questions?order=$(order)&sort=$(sort)&tagged=$(tag)&site=$(site)"; verbose=3)
+    end
+    compressed = HTTP.payload(r);
+
+    decompressed = transcode(GzipDecompressor, compressed);
+
+    json = JSON.parse(IOBuffer(decompressed))
+
+    for (k, v) in json
+        if occursin("items", k)
+            for item in v
+
+                link                = Vector()
+                view_count          = Vector()
+                creation_date       = Vector()
+                is_answered         = Vector()
+                owner               = Vector()
+                last_activity_date  = Vector()
+                score               = Vector()
+                accepted_answer_id  = Vector()
+                question_id         = Vector()
+                tags                = Vector()
+                title               = Vector()
+                answer_count        = Vector()
+
+                for (key, value) in item
+                    if occursin("link", key)
+                        push!(link, value)
+                    elseif occursin("view_count", key)
+                        push!(view_count, value)
+                    elseif occursin("creation_date", key)
+                        push!(creation_date, value)
+                    elseif occursin("is_answered", key)
+                        push!(is_answered, value)
+                    elseif occursin("owner", key)
+                        push!(owner, value)
+                    elseif occursin("last_activity_date", key)
+                        push!(last_activity_date, value)
+                    elseif occursin("score", key)
+                        push!(score, value)
+                    elseif occursin("accepted_answer_id", key)
+                        push!(accepted_answer_id, value)
+                    elseif occursin("question_id", key)
+                        push!(question_id, value)
+                    elseif occursin("tags", key)
+                        push!(tags, value)
+                    elseif occursin("title", key)
+                        push!(title, value)
+                    elseif occursin("answer_count", key)
+                        push!(answer_count, value)
+                    end
+                    # println(key)
+                end
+                question = Questions(link, view_count, creation_date, is_answered, owner,
+                    last_activity_date, score, accepted_answer_id, question_id, tags, title,
+                    answer_count)
+
+                push!(questionholder, question)
+            end
+
+            return questionholder
+        end
+    end
+
+end
+
+#fromdate="1573776000", todate="1574545174"
+qHolder = getrecentquestionsfortag()
