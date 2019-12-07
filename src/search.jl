37b02456dd9fc1c87b52a1fdefc793002feb7cc5
Kim Fung <fungtszto@yahoo.com>
2019-12-07 14:44:19 +0000

    Update search.jl

--- src/search.jl
+++ src/search.jl
@@ -1,11 +1,10 @@
+using StackOverflow
+
 """
     searcherror(intitle::String)
 
 Adding the ability to search for an error through StackOverflow.jl while in the Julia Terminal.
 """
-
-using StackOverflow
-
 function searcherror(intitle::String)
     data = replace(intitle, " " => "%20")
     r = HTTP.request("GET", "https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=$(data)&site=stackoverflow")
